0cd8a6923bb207f7d321afff30c1edd22be8dcf5
lukelloydjones <luke.lloydjones@uqconnect.edu.au>
2017-10-12 15:44:53 +1000

    Added better error checking and reporting of NAs

--- shiny_lmor_func.R
+++ shiny_lmor_func.R
@@ -27,13 +27,13 @@ LmToOddsRatio <- function(lmm.fil, k, std.err)
   df.names <- colnames(lmm.fil) 
   # Get the column name for the allele frequency 
   p.col <- which(df.names == "Freq" | df.names == "FREQ" | df.names == "FRQ" | 
-                 df.names == "A1FREQ" | df.names == "af"| df.names == "freq")
+                   df.names == "A1FREQ" | df.names == "af"| df.names == "freq")
   # If one of the column names is not in the set of possible names request the user to
   # change the column name
   if (length(p.col) == 0)
   {
-  	stop("Please rename the allele frequency column FREQ or add an allele frequency for the effect allele from a reference.
-  	      Alternatively, check that you have the correct seperator for your results file.")
+    stop("Please rename the allele frequency column FREQ or add an allele frequency for the effect allele from a reference.
+         Alternatively, check that you have the correct seperator for your results file.")
   }
   # --------------------------------------------------- 
   # Get the column name for the reference allele effect
@@ -43,23 +43,23 @@ LmToOddsRatio <- function(lmm.fil, k, std.err)
   # change the column name
   if (length(p.beta) == 0)
   {
-  	stop("Please rename the genetic effect column BETA or check that you have the correct seperator for your results file")
+    stop("Please rename the genetic effect column BETA or check that you have the correct seperator for your results file")
   }
   if (length(p.beta) > 1)
   {
-  	stop("You have multiple genetic coefficent columns please just submit one")
+    stop("You have multiple genetic coefficent columns please just submit one")
   }
   # Check to see if the effects have any NAs. Remove and notify user
   if (length(which(is.na(lmm.fil[, p.beta]))) != 0)
   {
-  	print("Removing results with NAs for the genetic effect")
-  	lm.fil <- lmm.fil[-which(is.na(lmm.fil[, p.beta])), ]
+    print("Removing results with NAs for the genetic effect")
+    lm.fil <- lmm.fil[-which(is.na(lmm.fil[, p.beta])), ]
   }
   # Check to see if the allele frequencies have any NAs. Remove and notify user
   if (length(which(is.na(lmm.fil[, p.col]))) != 0)
   {
-  	print("Removing results with no allele frequency")
-  	lm.fil <- lmm.fil[-which(is.na(lmm.fil[, p.beta])), ]
+    print("Removing results with no allele frequency")
+    lm.fil <- lmm.fil[-which(is.na(lmm.fil[, p.beta])), ]
   }
   # Set the variables for the allele and the effect
   p    <- lmm.fil[, p.col]
@@ -87,70 +87,70 @@ LmToOddsRatio <- function(lmm.fil, k, std.err)
   or.map.gld <- array(0, length(p))
   if (length(which(is.na(c3))) != 0 & length(which(is.na(f0))) != 0)
   {
-  	 # -------------------
-     # Odds ratio quadratic
-     # --------------------  
-     p.0.1 <- (-c2 + sqrt(c2 ^ 2 - 4 * c1 * c3)) / (2 * c1)
-     p.0.2 <- (-c2 - sqrt(c2 ^ 2 - 4 * c1 * c3)) / (2 * c1)
-     # There should be only one solution in (0, 1). Take the set that has all elements in (0, 1)
-     if (all(p.0.1 > 0 & p.0.1 < 1))
-     {
-       p_0 <- p.0.1
-     } else if (all(p.0.2 > 0 & p.0.2 < 1))
-     {
-  	   p_0 <- p.0.2
-     }
-     p_1 <- (p - (1 - k) *  p_0 ) / k  
-     or.map.gld  <- (p_1 * (1 - p_0)) / (p_0 * (1 - p_1))
-     or.map.gld[which(is.na(c3))] <- NA
-     or.map.gld[which(is.na(f0))]  <- NA   
+    # -------------------
+    # Odds ratio quadratic
+    # --------------------  
+    p.0.1 <- (-c2 + sqrt(c2 ^ 2 - 4 * c1 * c3)) / (2 * c1)
+    p.0.2 <- (-c2 - sqrt(c2 ^ 2 - 4 * c1 * c3)) / (2 * c1)
+    # There should be only one solution in (0, 1). Take the set that has all elements in (0, 1)
+    if (all(p.0.1 > 0 & p.0.1 < 1))
+    {
+      p_0 <- p.0.1
+    } else if (all(p.0.2 > 0 & p.0.2 < 1))
+    {
+      p_0 <- p.0.2
+    }
+    p_1 <- (p - (1 - k) *  p_0 ) / k  
+    or.map.gld  <- (p_1 * (1 - p_0)) / (p_0 * (1 - p_1))
+    or.map.gld[which(is.na(c3))] <- NA
+    or.map.gld[which(is.na(f0))]  <- NA   
   } else if (!all(beta > lb & beta < ub))  
   {
-  	 c1[which(beta < lb | beta> ub)] <- NA
-  	 c2[which(beta < lb | beta> ub)] <- NA
-  	 c3[which(beta < lb | beta> ub)] <- NA
-     p.0.1 <- (-c2 + sqrt(c2 ^ 2 - 4 * c1 * c3)) / (2 * c1)
-     p.0.2 <- (-c2 - sqrt(c2 ^ 2 - 4 * c1 * c3)) / (2 * c1)
-     # There should be only one solution in (0, 1). Take the set that has all elements in (0, 1)
-     if (!all(is.na(p.0.1)) & !all(is.na(p.0.1)))
-     {
-       if (all(p.0.1 > 0 & p.0.1 < 1))
-       {
-         p_0 <- p.0.1
-       } else if (all(p.0.2 > 0 & p.0.2 < 1))
-       {
-         p_0 <- p.0.2
-       }
-     } else
-     {
-     	p_0 <- NA
-     }
-     p_1 <- (p - (1 - k) *  p_0 ) / k
-  	 or.map.gld  <- (p_1 * (1 - p_0)) / (p_0 * (1 - p_1))
+    c1[which(beta < lb | beta> ub)] <- NA
+    c2[which(beta < lb | beta> ub)] <- NA
+    c3[which(beta < lb | beta> ub)] <- NA
+    p.0.1 <- (-c2 + sqrt(c2 ^ 2 - 4 * c1 * c3)) / (2 * c1)
+    p.0.2 <- (-c2 - sqrt(c2 ^ 2 - 4 * c1 * c3)) / (2 * c1)
+    # There should be only one solution in (0, 1). Take the set that has all elements in (0, 1)
+    if (!all(is.na(p.0.1)) & !all(is.na(p.0.1)))
+    {
+      if (all(p.0.1[-which(is.na(p.0.1))] > 0 & p.0.1[-which(is.na(p.0.1))] < 1))
+      {
+        p_0 <- p.0.1
+      } else if (all(p.0.2[-which(is.na(p.0.2))] > 0 & p.0.2[-which(is.na(p.0.2))] < 1))
+      {
+        p_0 <- p.0.2
+      }
+    } else
+    {
+      p_0 <- NA
+    }
+    p_1 <- (p - (1 - k) *  p_0 ) / k
+    or.map.gld  <- (p_1 * (1 - p_0)) / (p_0 * (1 - p_1))
   } else
   {
-  	 # If there are no concerns with NAs and ORs on the boundary
-  	 # calculate the full set of results
-     or.map.gld  <- (p_1 * (1 - p_0)) / (p_0 * (1 - p_1))
+    # If there are no concerns with NAs and ORs on the boundary
+    # calculate the full set of results
+    or.map.gld  <- (p_1 * (1 - p_0)) / (p_0 * (1 - p_1))
   }
   # ---------------------------------------------------------------
   # If standard error mappings are wanted
   # ---------------------------------------------------------------
   if (std.err == 1)
   {
-  	p.se <- which(df.names == "SE" | df.names == "se")
-  	p.n  <- which(df.names == "N" | df.names == "NMISS" | df.names == "nmiss")
+    p.se <- which(df.names == "SE" | df.names == "se")
+    p.n  <- which(df.names == "N" | df.names == "NMISS" | df.names == "nmiss")
     # If one of the column name is not in the set of possible names request the user to
     # change the column name
     if (length(p.se) == 0)
     {
-  	  stop("Please rename the standard error of the genetic effect column SE or check 
-  	        that you have the correct seperator for your results file")
+      stop("Please rename the standard error of the genetic effect column SE or check 
+           that you have the correct seperator for your results file")
     }
     if (length(p.n) == 0)
     {
-  	  stop("Please rename the number of individuals column N or check 
-  	        that you have the correct seperator for your results file")
+      stop("Please rename the number of individuals column N or check 
+           that you have the correct seperator for your results file")
     }
     se <- lmm.fil[, p.se]
     n  <- lmm.fil[, p.n]
@@ -173,13 +173,13 @@ LmToOddsRatio <- function(lmm.fil, k, std.err)
     sd <- var^2 * a + var * b + c
     if (!all(sd > 0))
     {
-    	or.est.2[which(sd < 0)] <- NA
+      or.est.2[which(sd < 0)] <- NA
     }
     # Return the results with the SE mapping
     res <- data.frame(lmm.fil,  or.est.2)
     colnames(res) <- c(colnames(lmm.fil), "OR_SE")
     return(res)
-  }
+    }
   # -------------------
   # Bind up the results 
   # -------------------
@@ -189,8 +189,7 @@ LmToOddsRatio <- function(lmm.fil, k, std.err)
     colnames(res) <- c(colnames(lmm.fil), "OR")
     return(res)
   }
-}
-
+  }
 
 
 
